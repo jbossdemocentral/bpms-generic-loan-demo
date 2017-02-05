4d4c87f1f9251bfc902fa89370034dbdf5fe681f
Eric D. Schabell <eric@schabell.org>
2017-02-05 23:17:53 +0100

    Updated to bpm suite 6.4.

--- init.bat
+++ init.bat
@@ -5,20 +5,19 @@ set PROJECT_HOME=%~dp0
 set DEMO=Generic Loan Demo
 set AUTHORS=Dan-Grigore Pupaz, Andrew Block,
 set AUTHORS2=Alexandre Porcelli, Eric D. Schabell, Duncan Doyle
-set PROJECT=git@github.com:jbossdemocentral/bpms-generic-loan-demo.git
+set PROJECT=git@github.com:eschabell/bpms-generic-loan-demo.git
 set PRODUCT=JBoss BPM Suite
 set TARGET_DIR=%PROJECT_HOME%target
-set JBOSS_HOME=%PROJECT_HOME%target\jboss-eap-6.4\
+set JBOSS_HOME=%PROJECT_HOME%target\jboss-eap-7.0\
 set SERVER_DIR=%JBOSS_HOME%standalone\deployments\
 set SERVER_CONF=%JBOSS_HOME%standalone\configuration\
 set SERVER_BIN=%JBOSS_HOME%bin
 set SRC_DIR=%PROJECT_HOME%installs
 set SUPPORT_DIR=%PROJECT_HOME%support
 set PRJ_DIR=%PROJECT_HOME%projects\bpms-generic-loan
-set BPMS=jboss-bpmsuite-6.3.0.GA-installer.jar
-set EAP=jboss-eap-6.4.0-installer.jar
-set EAP_PATCH=jboss-eap-6.4.7-patch.zip
-set VERSION=6.3
+set BPMS=jboss-bpmsuite-6.4.0.GA-deployable-eap7.x.zip
+set EAP=jboss-eap-7.0.0-installer.jar
+set VERSION=6.4
 
 REM wipe screen.
 cls
@@ -47,7 +46,7 @@ echo.
 
 REM make some checks first before proceeding.	
 if exist %SRC_DIR%\%EAP% (
-        echo Product sources are present...
+        echo Product EAP sources are present...
         echo.
 ) else (
         echo Need to download %EAP% package from the Customer Support Portal
@@ -56,18 +55,8 @@ if exist %SRC_DIR%\%EAP% (
         GOTO :EOF
 )
 
-if exist %SRC_DIR%\%EAP_PATCH% (
-        echo Product patches are present...
-        echo.
-) else (
-        echo Need to download %EAP_PATCH% package from the Customer Support Portal
-        echo and place it in the %SRC_DIR% directory to proceed...
-        echo.
-        GOTO :EOF
-)
-
 if exist %SRC_DIR%\%BPMS% (
-        echo Product sources are present...
+        echo Product BPM Suite sources are present...
         echo.
 ) else (
         echo Need to download %BPMS% package from the Customer Support Portal
@@ -96,40 +85,22 @@ if not "%ERRORLEVEL%" == "0" (
 	GOTO :EOF
 )
 
-pause
-call set NOPAUSE=true
 
-echo.
-echo Applying JBoss EAP patch now...
-echo.
-call %JBOSS_HOME%\bin\jboss-cli.bat --command="patch apply %SRC_DIR%/%EAP_PATCH% --override-all"
-
-if not "%ERRORLEVEL%" == "0" (
-  echo.
-	echo Error Occurred During JBoss EAP Patch Installation!
-	echo.
-	GOTO :EOF
-)
-pause
 echo.
 echo BPM Suite installer running now...
 echo.
-call java -jar %SRC_DIR%\%BPMS% %SUPPORT_DIR%\installation-bpms -variablefile %SUPPORT_DIR%\installation-bpms.variables
+cscript /nologo %SUPPORT_DIR%\unzip.vbs %SRC_DIR%\%BPMS% %PROJECT_HOME%\target
 
 if not "%ERRORLEVEL%" == "0" (
-  echo.
-	echo Error Occurred During JBoss BPM Suite Installation!
+	echo Error Occurred During %PRODUCT% Installation!
 	echo.
 	GOTO :EOF
 )
-pause
-echo - setting up demo projects...
-echo.
 
-echo - enabling demo accounts role setup in application-roles.properties file...
 echo.
-xcopy /Y /Q "%SUPPORT_DIR%\application-roles.properties" "%SERVER_CONF%"
-echo. 
+echo - enabling demo accounts role setup...
+echo.
+call %JBOSS_HOME%/bin/add-user.bat -a -r ApplicationRealm -u erics -p bpmsuite1! -ro analyst,admin,manager,user,kie-server,kiemgmt,rest-all --silent
 
 mkdir "%SERVER_BIN%\.niogit\"
 xcopy /Y /Q /S "%SUPPORT_DIR%\bpm-suite-demo-niogit\*" "%SERVER_BIN%\.niogit\"
@@ -156,17 +127,5 @@ xcopy /Y /Q "%SUPPORT_DIR%\userinfo.properties" "%SERVER_DIR%\business-central.w
 echo.
 echo You can now start the %PRODUCT% with %SERVER_BIN%\standalone.bat
 echo.
-
-echo PRE-LOAD DEMO
-echo =============
-echo To load the BPM with a set of process instances, you can run the following
-echo command after you start JBoss BPM Suite, build and deploy the mortgage
-echo project, then you can use the helper jar file found in the support directory
-echo as follows:
-echo. 
-echo    java -jar jboss-generic-loan-demo-client.jar erics bpmsuite1!
-echo.
-
-
 echo %PRODUCT% %VERSION% %DEMO% Setup Complete.
 echo.
--- init.sh
+++ init.sh
@@ -5,17 +5,16 @@ AUTHORS2="Alexandre Porcelli, Eric D. Schabell, Duncan Doyle"
 PROJECT="git@github.com:jbossdemocentral/bpms-generic-loan-demo.git"
 PRODUCT="JBoss BPM Suite"
 TARGET_DIR=./target
-JBOSS_HOME=./target/jboss-eap-6.4
+JBOSS_HOME=./target/jboss-eap-7.0
 SERVER_DIR=$JBOSS_HOME/standalone/deployments/
 SERVER_CONF=$JBOSS_HOME/standalone/configuration/
 SERVER_BIN=$JBOSS_HOME/bin
 SRC_DIR=./installs
 SUPPORT_DIR=./support
 PRJ_DIR=./projects/bpms-generic-loan
-BPMS=jboss-bpmsuite-6.3.0.GA-installer.jar
-EAP=jboss-eap-6.4.0-installer.jar
-EAP_PATCH=jboss-eap-6.4.7-patch.zip
-VERSION=6.3
+BPMS=jboss-bpmsuite-6.4.0.GA-deployable-eap7.x.zip
+EAP=jboss-eap-7.0.0-installer.jar
+VERSION=6.4
 
 # wipe screen.
 clear 
@@ -46,7 +45,7 @@ command -v mvn -q >/dev/null 2>&1 || { echo >&2 "Maven is required but not insta
 
 # make some checks first before proceeding.	
 if [ -r $SRC_DIR/$EAP ] || [ -L $SRC_DIR/$EAP ]; then
-	echo Product sources are present...
+	echo Product EAP sources are present...
 	echo
 else
 	echo Need to download $EAP package from the Customer Portal 
@@ -55,18 +54,8 @@ else
 	exit
 fi
 
-if [ -r $SRC_DIR/$EAP_PATCH ] || [ -L $SRC_DIR/$EAP_PATCH ]; then
-	echo Product patches are present...
-	echo
-else
-	echo Need to download $EAP_PATCH package from the Customer Portal 
-	echo and place it in the $SRC_DIR directory to proceed...
-	echo
-	exit
-fi
-
 if [ -r $SRC_DIR/$BPMS ] || [ -L $SRC_DIR/$BPMS ]; then
-		echo Product sources are present...
+		echo Product BPM Suite sources are present...
 		echo
 else
 		echo Need to download $BPMS package from the Customer Portal 
@@ -94,29 +83,18 @@ if [ $? -ne 0 ]; then
 fi
 
 echo
-echo "Applying JBoss EAP patch now..."
-echo
-$JBOSS_HOME/bin/jboss-cli.sh --command="patch apply $SRC_DIR/$EAP_PATCH"
-
-if [ $? -ne 0 ]; then
-	echo "Error occurred during JBoss EAP patching!"
-	exit
-fi
-
-echo
 echo "JBoss BPM Suite installer running now..."
 echo
-java -jar $SRC_DIR/$BPMS $SUPPORT_DIR/installation-bpms -variablefile $SUPPORT_DIR/installation-bpms.variables
+unzip -qo $SRC_DIR/$BPMS -d ./target
 
 if [ $? -ne 0 ]; then
 	echo "Error occurred during JBoss BPM Suite installation!"
 	exit
 fi
 
+echo "  - enabling demo accounts setup ..."
 echo
-echo "  - enabling demo accounts role setup in application-roles.properties file..."
-echo
-cp $SUPPORT_DIR/application-roles.properties $SERVER_CONF
+$JBOSS_HOME/bin/add-user.sh -a -r ApplicationRealm -u erics -p bpmsuite1! -ro analyst,admin,manager,user,kie-server,kiemgmt,rest-all --silent
 
 echo "  - setting up demo projects..."
 echo
@@ -140,20 +118,18 @@ chmod u+x $JBOSS_HOME/bin/standalone.sh
 #cp $SUPPORT_DIR/1000_jbpm_demo_h2.sql $SERVER_DIR/dashbuilder.war/WEB-INF/etc/sql
 #echo
 
-echo "You can now start the $PRODUCT with $SERVER_BIN/standalone.sh"
 echo
-echo "Login into business central at:"
-echo
-echo "    http://localhost:8080/business-central  (u:erics / p:bpmsuite1!)"
-echo
-echo "PRE-LOAD DEMO"
-echo "============="
-echo "To load the BPM with a set of process instances, you can run the following command"
-echo "after you start JBoss BPM Suite, build and deploy the mortgage project, then you can"
-echo "use the helper jar file found in the support directory as follows:"
-echo 
-echo "   java -jar $SUPPORT_DIR/jboss-generic-loan-demo-client.jar erics bpmsuite1!" 
-echo
-echo "$PRODUCT $VERSION $DEMO Setup Complete."
-echo
+echo "========================================================================"
+echo "=                                                                      ="
+echo "=  You can now start the $PRODUCT with:                         ="
+echo "=                                                                      ="
+echo "=   $SERVER_BIN/standalone.sh                           ="
+echo "=                                                                      ="
+echo "=  Login into business central at:                                     ="
+echo "=                                                                      ="
+echo "=    http://localhost:8080/business-central  (u:erics / p:bpmsuite1!)  ="
+echo "=                                                                      ="
+echo "=  $PRODUCT $VERSION $DEMO Setup Complete.               ="
+echo "=                                                                      ="
+echo "========================================================================"
 
